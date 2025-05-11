54735436200e84149976ccb335f6cddd62ff436e
Noom <segovia.network@gmail.com>
2025-05-10 23:58:47 -0500

    update

--- src/main/java/com/example/demo/CorsConfig.java
+++ src/main/java/com/example/demo/CorsConfig.java
@@ -3,7 +3,8 @@ package com.example.demo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
@@ -13,8 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+                        .allowedOriginPatterns("*") // Usar esto en lugar de allowedOrigins("*") para evitar advertencias con credenciales
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
